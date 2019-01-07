21c91c286029c01d19c2536c2c4bbffc5ca8247c
George Zhou <georgezhou@augmentum.com.cn>
2019-01-07 18:59:37 +0800

    adds markdown

---
+++ index.js
@@ -0,0 +1,6 @@
+var DeletionExtraDataJson = {
+    TargetSyncIdList : ['0b5a64a6-524c-4fc0-b372-aa13d608f948', '0b5a64a6-524c-4fc0-b372-aa13d608f949']
+}
+
+var jsonString = JSON.stringify(DeletionExtraDataJson)
+console.log(jsonString)
